289ed919a674c517c2af50c845b8a8fda6d4f49e
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2023-09-12 14:20:13 +0200

    POO ACT1 EX1

---
+++ POO/ACT01/6TTI_POOACT01EX01_AY/Chien.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_POOACT01EX01_AY
+{
+    internal class Chien
+    {
+        private string _nom;
+        private string _race;
+        private uint _age;
+        private string _genre;
+        private bool _estVaccine;
+
+        public uint Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+
+        public Chien(string nom, string race, uint age, bool estVaccine, string genre)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+            _genre = genre;
+            _estVaccine = estVaccine;
+        }
+
+        public void Vaccine()
+        {
+            _estVaccine = true;
+        }
+
+        public string AfficheCaracteristiques()
+        {
+            return "Chien[Nom: " + _nom + "; " + "Race: " + _race + "; " + "Age: " + _age + "; " + "Genre: " + _genre + "; " + "EstVaccine: " + _estVaccine + "]";
+        }
+    }
+}
---
+++ POO/ACT01/6TTI_POOACT01EX01_AY/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _6TTI_POOACT01EX01_AY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chien[] mesChiens = new Chien[3];
+
+            for (int i = 0; i < mesChiens.Length; i++)
+            {
+                Chien monChien = new Chien("Chien" + (i + 1), "Golden Retriever", 0, false, i % 2 == 0 ? "f" : "m");
+                Console.WriteLine(monChien.AfficheCaracteristiques());
+                monChien.Vaccine();
+                monChien.Age++;
+                Console.WriteLine(monChien.AfficheCaracteristiques() + "\n");
+                mesChiens[i] = monChien;
+            }
+
+            Console.WriteLine("J'ai " + mesChiens.Length + " chiens dans mon tableau.");
+        }
+    }
+}
