f73032464025b308dd5a2a6ad460a39b53a17132
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2023-09-26 14:14:28 +0200

    POO FORM2

---
+++ POO/FORM02/I2P623_Aymeric/FeuSignalisation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P623_Aymeric
+{
+    internal class FeuSignalisation
+    {
+        private string[] couleurs = { "Vert", "Orange", "Rouge" };
+
+        // Caractéristiques
+        private string _id; // identifiant qui permet de reconnaître le feu
+        private int _couleur; // couleur actuelle du feu (0 = vert; 1 = orange; 2 = rouge)
+        private bool _allume; // si le feu est allumé ou pas
+
+        // Constructeur
+        public FeuSignalisation(string id, int couleur, bool allume)
+        {
+            _id = id;
+            _couleur = couleur;
+            _allume = allume;
+        }
+
+        // Propriétés
+        public string Id
+        {
+            get { return _id; }
+        }
+
+        public int Couleur
+        {
+            get { return _couleur; }
+        }
+
+        public bool Allume
+        {
+            get { return _allume; }
+        }
+
+        // Méthodes
+
+        /// <summary>
+        /// Changer la couleur du feu de signalisation
+        /// </summary>
+        /// <param name="couleur">La nouvelle couleur du feu</param>
+        /// <returns>Vrai si la permutation a réussi</returns>
+        public bool changerCouleur(int couleur)
+        {
+            _couleur = couleur;
+            return true;
+        }
+
+        /// <summary>
+        /// Faire clignoter le feu de signalisation
+        /// </summary>
+        /// <returns>Vrai si le clignotement a réussi</returns>
+        public bool clignoter()
+        {
+            _allume = !_allume;
+            return true;
+        }
+
+        /// <summary>
+        /// Afficher les caractéristiques du feu
+        /// </summary>
+        /// <returns>Un string contenant les caracéristiques du feu</returns>
+        public string AfficherCaracteristiques()
+        {
+            return "Le feu de signalisation " + _id + " est " + couleurs[_couleur] + " et est " + (_allume ? "allumé" : "éteint");
+        }
+    }
+}
---
+++ POO/FORM02/I2P623_Aymeric/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace I2P623_Aymeric
+{
+    internal class Program
+    {
+        static ConsoleColor[] couleursConsole = { ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Red };
+
+        static void Main(string[] args)
+        {
+            // Initialisation des feux
+            FeuSignalisation feuSignalisation1 = new FeuSignalisation("1001", 0, true);
+            FeuSignalisation feuSignalisation2 = new FeuSignalisation("007", 2, true);
+
+            // Afficher l'état des feux
+            Console.WriteLine("Etat des feux :");
+            Console.WriteLine("----------------------");
+            AfficherAvecCouleur(feuSignalisation1);
+            AfficherAvecCouleur(feuSignalisation2);
+            
+            // Tester le changement d'état
+            Console.WriteLine("\nChangement d'état :");
+            Console.WriteLine("----------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                feuSignalisation1.changerCouleur((feuSignalisation1.Couleur + 1) % 3);
+                AfficherAvecCouleur(feuSignalisation1);
+            }
+
+            Console.WriteLine("\nFaire passer le " + feuSignalisation2.Id + " à l'orange :");
+            feuSignalisation2.changerCouleur(1);
+            AfficherAvecCouleur(feuSignalisation2);
+            Console.WriteLine("----------------------");
+
+            // Tester le clignotement des feux
+            Console.WriteLine("\nFeu clignotant :");
+            Console.WriteLine("----------------------");
+            for (int i = 0; i < 7; i++)
+            {
+                feuSignalisation2.clignoter();
+                AfficherAvecCouleur(feuSignalisation2);
+            }
+        }
+
+        /// <summary>
+        /// Afficher les caractéristiques d'un feu dans la console avec des couleurs
+        /// </summary>
+        /// <param name="feuSignalisation">Feu à afficher dans la console</param>
+        static void AfficherAvecCouleur(FeuSignalisation feuSignalisation)
+        {
+            Console.ForegroundColor = couleursConsole[feuSignalisation.Couleur];
+            Console.BackgroundColor = feuSignalisation.Allume ? ConsoleColor.DarkGray : ConsoleColor.Gray;
+            Console.WriteLine(feuSignalisation.AfficherCaracteristiques());
+            Console.ResetColor();
+        }
+    }
+}
