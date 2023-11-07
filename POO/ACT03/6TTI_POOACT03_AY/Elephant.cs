d38ea343821518bab10ede157caf3f91f7328ad4
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2023-11-07 13:59:30 +0100

    POO ACT3

---
+++ POO/ACT03/6TTI_POOACT03_AY/Elephant.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6TTI_POOACT03_AY
+{
+    internal class Elephant
+    {
+        private string _nom;
+        private uint _tailleOreilles;
+
+        public Elephant(string nom, uint tailleOreilles)
+        {
+            _nom = nom;
+            _tailleOreilles = tailleOreilles;
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+        }
+
+        public uint TailleOreilles
+        {
+            get { return _tailleOreilles; }
+        }
+
+        
+        /// <summary>
+        /// Afficher les caractéristiques de l'éléphant
+        /// </summary>
+        /// <returns>Le nom et la taille des oreilles de l'éléphant</returns>
+        public string AfficheQuiJeSuis()
+        {
+            return "Je m'appelle " + _nom + ".\nMes oreilles mesurent " + _tailleOreilles + "cm.";
+        }
+
+        /// <summary>
+        /// Rcevoir un message d'un autre éléphant
+        /// </summary>
+        /// <param name="message">Le message de l'autre éléphant</param>
+        /// <param name="quiDit">L'éléphant qui envoie le message</param>
+        public void EcouteMessage(string message, Elephant quiDit) {
+            Console.WriteLine(_nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+        }
+
+        /// <summary>
+        /// Envoyer un message à un autre éléphant
+        /// </summary>
+        /// <param name="message">Le message que l'éléhant doit envoyer</param>
+        /// <param name="quiRecoit">L'éléphant qui doit recevoir le message</param>
+        public void EnvoieMessage(string message, Elephant quiRecoit)
+        {
+            quiRecoit.EcouteMessage(message, this);
+        }
+    }
+}
---
+++ POO/ACT03/6TTI_POOACT03_AY/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace _6TTI_POOACT03_AY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Message de bienvenue et affichages des choix disponibles
+            Console.WriteLine("Bienvenue dans cet échange d'éléphants");
+            Console.WriteLine("Tapez :");
+            Console.WriteLine("1 pour afficher les informations de Zazou,");
+            Console.WriteLine("2 pour Titi,");
+            Console.WriteLine("3 pour les échanger");
+            Console.WriteLine("4 pour voir le transfert d'un message de Zazou à Titi");
+            Console.WriteLine("5 pour réviser la notion de tableaux et l'appliquer à des objets");
+            Console.WriteLine("6 pour quitter");
+
+            // Variable de recommencement et boucle du programme
+            bool recommencer = true;
+            do
+            {
+                // Obtenir le choix de l'utilisateur
+                int choix;
+                if (!int.TryParse(Console.ReadLine(), out choix))
+                {
+                    Console.WriteLine("Choix non valide");
+                } else
+                {
+                    // Instancier les éléphants
+                    Elephant zazou = new Elephant("Zazou", 82);
+                    Elephant titi = new Elephant("Titi", 100);
+
+                    switch (choix)
+                    {
+                        case 1:
+                            Console.WriteLine(zazou.AfficheQuiJeSuis());
+                            break;
+                        case 2:
+                            Console.WriteLine(titi.AfficheQuiJeSuis());
+                            break;
+                        case 3:
+                            // Echanger zazou et titi
+                            Elephant tampon = zazou;
+                            zazou = titi;
+                            titi = tampon;
+
+                            Console.WriteLine("Zazou: ");
+                            Console.WriteLine(zazou.AfficheQuiJeSuis());
+                            Console.WriteLine("Titi: ");
+                            Console.WriteLine(titi.AfficheQuiJeSuis());
+                            break;
+                        case 4:
+                            zazou.EnvoieMessage("Coucou Titi !", titi);
+                            break;
+                        case 5:
+                            // Création du tableau avec les éléphants
+                            Elephant[] elephants = new Elephant[7];
+                            elephants[0] = zazou;
+                            elephants[1] = titi;
+                            elephants[2] = new Elephant("Victor", 147);
+                            elephants[3] = new Elephant("Alexandre", 10);
+                            elephants[4] = new Elephant("Sefedin", 58);
+                            elephants[5] = new Elephant("Louis", 96);
+                            elephants[6] = new Elephant("Nathan", 69);
+
+                            // Déterminer l'éléphants avec les + grandes oreilles
+                            Elephant? max = null;
+                            for (int i = 0; i < elephants.Length; i++)
+                            {
+                                Elephant elephant = elephants[i];
+                                if (max == null || elephant.TailleOreilles > max.TailleOreilles)
+                                {
+                                    max = elephant;
+                                }
+                            }
+
+                            // Vérifier que l'éléphant n'est pas null
+                            if (max != null)
+                            {
+                                Console.WriteLine("L'éléphant qui a les plus grandes oreilles est " + max.Nom + " avec oreilles de " + max.TailleOreilles + "cm.");
+                            }
+                            break;
+                        case 6:
+                            recommencer = false;
+                            break;
+                        default:
+                            Console.WriteLine("Choix inconnu");
+                            break;
+                    }
+                }
+            } while(recommencer);
+        }
+    }
+}
