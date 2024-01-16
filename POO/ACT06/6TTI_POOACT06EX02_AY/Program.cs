b1019fbd3beff485622302f6fcf196f4d08a7aaa
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2024-01-16 13:40:43 +0100

    [FIX] POO ACT6 EX2

--- POO/ACT06/6TTI_POOACT06EX02_AY/Program.cs
+++ POO/ACT06/6TTI_POOACT06EX02_AY/Program.cs
@@ -4,13 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Chat chat = new Chat("Pitor", "01/01/2003", "D763", 30, false);
-            Chien chien = new Chien("Médor", "01/01/2002", "P893", 50, false);
-            Lapin lapin = new Lapin("Flocon", "01/01/2009", "C471", 10, true, 3);
+            Animal[] animals = new Animal[3];
+            animals[0] = new Chat("Pitor", "01/01/2003", "D763", 30, false);
+            animals[1] = new Chien("Médor", "01/01/2002", "P893", 50, false);
+            animals[2] = new Lapin("Flocon", "01/01/2009", "C471", 10, true, 3);
 
-            Console.WriteLine(chat.Afficher());
-            Console.WriteLine(chien.Afficher());
-            Console.WriteLine(lapin.Afficher());
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Animal animal = animals[i];
+                Console.WriteLine(animal.Afficher());
+                animal.Manger();
+                if (animal is Chat)
+                {
+                    ((Chat)animal).Miauler();
+                } else if (animal is Chien)
+                {
+                    ((Chien)animal).Aboyer();
+                } else if (animal is Lapin)
+                {
+                    ((Lapin)animal).FaireBond();
+                }
+                animal.Dormir();
+            }
         }
     }
 }
