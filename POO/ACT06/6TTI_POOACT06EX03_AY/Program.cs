9b52afbc5334b95c45a10bdce7aefc1c83d46dac
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2024-01-16 13:30:54 +0100

    POO ACT6 EX3

---
+++ POO/ACT06/6TTI_POOACT06EX03_AY/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _6TTI_POOACT06EX03_AY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carre carre = new Carre(3, "bleu");
+            Rectangle rectangle = new Rectangle(5, 3, "rouge");
+
+            Console.WriteLine(carre.Afficher());
+            Console.WriteLine(rectangle.Afficher());
+        }
+    }
+}
